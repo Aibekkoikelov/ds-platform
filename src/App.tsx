3d8483cbbac7aafcadb26b8d02296a470b2d6c6f
Aibek <aibekkoikelov@gmail.com>
2022-08-23 16:41:05 +0600

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import "./App.css"
+const App = () => {
+    const [state,setState]= useState('')
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default App;
