2091be2e8ed700f851dee3848fd311fce29380de
Мамед Джафаров <deff2666@gmail.com>
2022-08-31 10:35:51 +0600

    Register Auth Resurses

--- src/pages/lessonPage/components/LessonItem.tsx
+++ src/pages/lessonPage/components/LessonItem.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import style from "../leson.module.scss"
+import style from '../leson.module.scss';
 function LessonItem() {
-    return (
-        <div className={style.lesson_item}>
-          <div className={style.date_day}>{"05"}</div>
-            <div className={style.date_month}>{"Август"}</div>
-
-        </div>
-    );
+  return (
+    <div className={style.lesson_item}>
+      <div className={style.date_day}>{'05'}</div>
+      <div className={style.date_month}>{'Август'}</div>
+    </div>
+  );
 }
 
 export default LessonItem;
--- src/routes/constant.ts
+++
@@ -1,8 +0,0 @@
-
-export const MAIN_PAGE = "/"
-export const LOGIN_PAGE = "/login"
-export const ERROR_PAGE = "/error"
-export const NEWS_PAGE = "/news"
-export const LESSON = "/course"
-export const RESOURCE = "/resource"
-export const HOME_WORK = "/work"
